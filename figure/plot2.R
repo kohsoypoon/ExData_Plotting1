6e36bdfc47402ba0c4b008ad74da3ab2e6c3d0f4
kohsoypoon <itsassoc@gmail.com>
2014-08-11 04:36:09 +0800

    programming assignment 1

---
+++ figure/plot2.R
@@ -0,0 +1,5 @@
+ds <- read.table("household_power_consumption.txt", sep=";", header=TRUE, skip=66636, nrow=2880, stringsAsFactor=FALSE, col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+ds$Time <- strptime(paste(ds$Date,ds$Time), "%d/%m/%Y %H:%M:%S")
+png("plot2.png", width=480, height=480)
+plot (ds$Time, ds$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ figure/plot3.R
@@ -0,0 +1,8 @@
+ds <- read.table("household_power_consumption.txt", sep=";", header=TRUE, skip=66636, nrow=2880, stringsAsFactor=FALSE, col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+ds$Time <- strptime(paste(ds$Date,ds$Time), "%d/%m/%Y %H:%M:%S")
+png("plot3.png", width=480, height=480)
+plot (ds$Time, ds$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
+lines(ds$Time, ds$Sub_metering_2, type="l", col="red")
+lines(ds$Time, ds$Sub_metering_3, type="l", col="blue")
+legend(x="topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
+dev.off()
